b605d699a71743c894185cf9a42acceb33b9d56c
KollerJacq <147981831+KollerJacq@users.noreply.github.com>
2024-01-17 17:41:33 +0100

    Added another date format

--- date/script.js
+++ date/script.js
@@ -1,6 +1,6 @@
 let day = document.getElementById("day");
 let time = document.getElementById("time");
-
+let secondDate = document.getElementById("secondDate");
 let days = [
   "Sunday",
   "Monday",
@@ -12,6 +12,9 @@ let days = [
 ];
 
 const date = new Date();
+let currentMonth = date.getMonth() + 1;
+let currentYear = date.getFullYear();
+let secondDay = date.getDate();
 let currentDay = days[date.getDay()];
 let currentHour = date.getHours();
 let currentMinutes = date.getMinutes();
@@ -20,5 +23,23 @@ let currentTime = `${currentHour}:${currentMinutes}:${currentSeconds}`;
 console.log(currentTime);
 console.log(currentDay);
 
+if (currentMonth < 10) {
+  currentMonth = "0" + currentMonth;
+}
+if (secondDay < 10) {
+  secondDay = "0" + secondDay;
+}
+if (currentHour < 10) {
+  currentHour = "0" + currentHour;
+}
+if (currentMinutes < 10) {
+  currentMinutes = "0" + currentMinutes;
+}
+if (currentSeconds < 10) {
+  currentSeconds = "0" + currentSeconds;
+}
+
 day.innerHTML = `Today is: ${currentDay}`;
 time.innerHTML = `Current time: ${currentTime}`;
+
+secondDate.innerHTML = `${secondDay}-${currentMonth}-${currentYear}`;
